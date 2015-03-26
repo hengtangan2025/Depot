f4c629c6dbc0e7162ade426b4687cf5f6416a3a9
zhoujianbin <504371515@qq.com>
2015-03-26 14:57:37 +0800

    Validation!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,17 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  fixtures :products
+
+  test "product is not valid without a unique title - i18n" do  
+   product = Product.new(title:       products(:ruby).title,  
+                         description: "yyy",   
+                         price:       1,   
+                         image_url:   "fred.gif")  
+  
+   assert !product.save  
+   assert_equal ["has already been taken"], product.errors[:title]  
+  end  
 
   # Add more helper methods to be used by all tests here...
 end
