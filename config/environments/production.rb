088fb67ae5f249950fb867f9b189d0f3730c8dc2
zhoujianbin <504371515@qq.com>
2015-04-14 15:12:08 +0800

    13chapter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,17 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_mailer.delivery_method = :smtp 
 
+  config.action_mailer.smtp_settings = {
+    :address         => "smtp.gmail.com",
+    :port            => 587,
+    :domain          => "domain.of.sender.net",
+    :authentication  => "plain",
+    :user_name       => "dave",
+    :password        => "secret",
+    :enable_starttls => true
+  }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
